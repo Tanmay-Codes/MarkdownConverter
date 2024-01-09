63aab1f1e553b4f5594aca50e8728fb2210cb227
Tanmay Agrawal <tanmayagcode@gmail.com>
2024-01-10 00:18:06 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MarkComponent from './components/MarkComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <MarkComponent/>
+    </div>
+  );
+
+  }
+export default App;
---
+++ src/components/MarkComponent.js
@@ -0,0 +1,39 @@
+import { Marked } from 'marked'
+import React, { useState } from 'react'
+import Markdown from 'react-markdown'
+import { FaExpandArrowsAlt } from "react-icons/fa";
+
+function MarkComponent() {
+    const [value, setValue] = useState(null)
+    const [preview, setPreview] = useState(false)
+    const [editor, setEditor] = useState(false)
+  return (
+    <div className='container'>
+      <div className={editor ?'toggle-preview' :'editor-container shadow'}>
+        <div className='header-container' >
+          <span className='header-text' >Editor</span>
+          <span onClick={()=>setPreview(prev=> !prev)} >
+          <FaExpandArrowsAlt/>
+          </span>
+        </div>
+        <textarea id='editor' className={preview ?'toggle-height':''} onChange={(e)=>setValue(e.target.value)}>
+        </textarea>
+      </div>
+      <div className={preview ?'toggle-preview' :'preview-container shadow'} >
+      <div className='header-container' >
+          <span className='header-text' >Editor</span>
+          <span onClick={()=>setEditor(prev=>!prev)}>
+            <FaExpandArrowsAlt/>
+            </span>
+        </div>
+        <div id='preview'>
+            <Markdown >
+            { value}
+            </Markdown>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default MarkComponent  
